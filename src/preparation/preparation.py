9971047e60fd78d1440265888c344df99007603c
mgltorsa <mgltorsa@gmail.com>
2024-12-19 11:50:39 -0500

    updated the way on running experiments

--- src/preparation/preparation.py
+++ src/preparation/preparation.py
@@ -1,13 +1,42 @@
 import subprocess
 import os
-import sys
-import asyncio
 import os
 from models.experiments import Experiment
-from typing import List
+from typing import List, Dict
 import json
 
-def prepare_experiment(llm, prompt_name, prompt_path, experiment: Experiment, parameters={'temperature':0.4, "top_p":0.1}) -> List[Experiment]:
+def prepare_tiling_experiment(experiment: Experiment, tiling_parameters: Dict[str:str]) -> List[Experiment]:
+    benchmark = experiment.benchmark_folder
+    kernel = experiment.kernel_folder
+    routine_name = experiment.routine_name
+    
+    outdir = f"{benchmark}/{kernel}/{routine_name}/tiling"
+    
+    opts = []
+    
+    for key, value in tiling_parameters.items():
+        opts.append(f"-{key}={value}")
+    
+    opts_str = " ".join(opts)
+    # Build the command dynamically
+    options = f"-verbosity=3 -paw-tiling -outdir={outdir} {opts_str}"
+    
+    file_path = f"{benchmark}/{kernel}/{routine_name}.c"
+    command = f"echo $CPATH && pcaot_wsl {options} {file_path}"
+    
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    process.communicate()  # This waits for the process to finish and prints the output
+    output = process.stdout
+    errors = process.stderr
+    
+    compilable_experiment = Experiment(experiment.benchmark_type, experiment.trials, experiment.dataset, experiment.benchmark_folder,
+                                           experiment.kernel_folder, experiment.routine_name, experiment.binary_file, [], experiment.source_placeholder)
+    
+    return [compilable_experiment]
+    
+
+def prepare_llm_experiment(llm, prompt_name, prompt_path, experiment: Experiment, parameters={'temperature':0.4, "top_p":0.1}) -> List[Experiment]:
     # Define the options dynamically
     benchmark = experiment.benchmark_folder
     kernel = experiment.kernel_folder
@@ -64,53 +93,3 @@ def prepare_experiment(llm, prompt_name, prompt_path, experiment: Experiment, pa
         
     
     return compilable_experiments
-    
-    
-
-async def prepare_experiment_async(llms_opt, prompt_name, prompt_path, benchmark, subroutine, cpath, file_path,parameters={'temperature':0.4}):
-    try:
-        print("RUNNING EXPERIMENT")
-        await prepare_experiment(llms_opt, prompt_name, prompt_path, benchmark, subroutine, cpath, file_path, parameters)
-    except Exception as e:  
-        print("FROM_ASYNC",e)
-    
-    
-async def main():
-    if len(sys.argv) != 2:
-        print("Remember Usage: python script.py llm-opts? llm-prompts?")
-        
-    llm_opt = "gpt-4"
-    
-    prompts = {
-        "naive-tiler": "/mnt/d/workspace/ud-masters/research-ideas/LLM/use-cases/prompts/naive-tiler",
-        "augmented-naive-tiler":"/mnt/d/workspace/ud-masters/research-ideas/LLM/use-cases/prompts/augmented-naive-tiler",
-        "parallel-aware-tiler":"/mnt/d/workspace/ud-masters/research-ideas/LLM/use-cases/prompts/parallel-aware-tiler"
-    }
-    
-    if len(sys.argv) > 1:
-        llm_opt = sys.argv[1]
-        
-        
-    print("LLMS: "+llm_opt)
-    print("PROMPTS: "+prompts)
-    
-    with open("source_codes.txt", "r") as file:
-        source_code_infos = [line.split(",") for line in file.read().splitlines()]
-    
-    # temperatures = [0.1, 0.4, 0.7]
-    temperatures = [0.2]
-    topPs=[0.1]
-    tasks = []
-    # for i in range(3):
-    for benchmark, subroutine, cpath, file_path in source_code_infos:
-        print(f"Processing {benchmark}/{subroutine}:")
-        for temperature in temperatures:
-            for top_p in topPs:
-                parameters = {'temperature': temperature, "top_p":top_p}
-                for prompt_name, prompt_path in prompts.items():
-                    task = asyncio.create_task(prepare_experiment_async(llm_opt, prompt_name, prompt_path, benchmark, subroutine, cpath, file_path.strip(), parameters))
-                    tasks.append(task)
-            
-    await asyncio.gather(*tasks)
-
-asyncio.run(main())
--- src/run_all.py
+++ src/run_all.py
@@ -1,19 +1,41 @@
 from models.experiments import Experiment
-from preparation.preparation import prepare_experiment
+from preparation.preparation import prepare_llm_experiment, prepare_tiling_experiment
 from compilation.compiler import compile_experiment
 from experiments.experimenter import run_experiment as run_experimenter
+import sys
+from typing import Dict
 
-
-def run_baseline(experiment: Experiment):
+def _run_baseline(experiment: Experiment):
     runnable_experiment = compile_experiment(experiment)
     run_experimenter(runnable_experiment)
     pass
 
+def _run_cetus_experiment(aot, parameters, experiment: Experiment):
+    pass
 
-def run_experiment(llm, prompt_name, prompt_path, experiment: Experiment):
+def _run_llm_experiments(aot, parameters: Dict[str], experiment: Experiment):
+    total_compilable_experiments = []
+    
+    for prompt_name, prompt_path in parameters.items():
+        compilable_experiments = prepare_llm_experiment(aot, prompt_name, prompt_path, experiment)
+        total_compilable_experiments.append()
+    
+    
+    return total_compilable_experiments
 
-    compilable_experiments = prepare_experiment(
-        llm, prompt_name, prompt_path, experiment)
+def run_experiment(aot, parameters, experiment: Experiment):
+    
+    
+    compilable_experiments = []
+    aot_params = parameters[aot]
+    
+    if(aot == 'mock' or aot == 'gpt-4' or aot == 'llama'):
+        _run_llm_experiments(aot, aot_params, experiment)
+    
+    if aot == 'cetus-tiling':
+        _run_llm_experiments(aot, aot_params, experiment)
+
+        
     for compilable_experiment in compilable_experiments:
         runnable_experiment = compile_experiment(compilable_experiment)
         run_experimenter(runnable_experiment)
@@ -21,25 +43,41 @@ def run_experiment(llm, prompt_name, prompt_path, experiment: Experiment):
 
 
 if __name__ == "__main__":
+    
+    experiment_plans_file_path_from_args = "experiment_plans.csv"
+    if len(sys.argv) > 1:
+        experiment_plans_file_path_from_args = sys.argv[1]
 
-    with open("experiment_plans.txt", "r") as file:
-        source_code_infos = [line.split(",")
+    with open(experiment_plans_file_path_from_args, "r") as file:
+        source_code_infos = [line.split(";")
                              for line in file.read().splitlines()[1:]]
 
-    aots = ['mock']
-   # prompt_approaches = []
-    prompt_approaches = {
-        "naive-tiler": "/mnt/d/workspace/ud-masters/research-ideas/LLM/use-cases/prompts/naive-tiler",
-        "augmented-naive-tiler": "/mnt/d/workspace/ud-masters/research-ideas/LLM/use-cases/prompts/augmented-naive-tiler",
-        "parallel-aware-tiler": "/mnt/d/workspace/ud-masters/research-ideas/LLM/use-cases/prompts/parallel-aware-tiler"
+    aots = ['mock','cetus-tiling', 'cetus']
+    # prompt_approaches = []
+    parameters = {
+        "mock":{
+            "naive-tiler": "/mnt/d/workspace/ud-masters/research-ideas/LLM/use-cases/prompts/naive-tiler",
+            "augmented-naive-tiler": "/mnt/d/workspace/ud-masters/research-ideas/LLM/use-cases/prompts/augmented-naive-tiler",
+            "parallel-aware-tiler": "/mnt/d/workspace/ud-masters/research-ideas/LLM/use-cases/prompts/parallel-aware-tiler"
+        },
+        "gpt-4": {
+            "naive-tiler": "/mnt/d/workspace/ud-masters/research-ideas/LLM/use-cases/prompts/naive-tiler",
+            "augmented-naive-tiler": "/mnt/d/workspace/ud-masters/research-ideas/LLM/use-cases/prompts/augmented-naive-tiler",
+            "parallel-aware-tiler": "/mnt/d/workspace/ud-masters/research-ideas/LLM/use-cases/prompts/parallel-aware-tiler"
+        },
+        "cetus-tiling": {
+            "tiling-level": "2",
+        },
+        "cetus":{
+            
+        }
+        
     }
-    # prompt_approaches = ["SIP", ""]
 
     for benchmark_type, trials, dataset, benchmark_folder, kernel_folder, routine_name, source_placeholder, binary_file in source_code_infos:
         baseline_comp_flags = ["SERIAL"]
         experiment = Experiment(benchmark_type, trials, dataset, benchmark_folder.strip(), kernel_folder.strip(
         ), routine_name.strip(), binary_file.strip(), baseline_comp_flags, source_placeholder.strip())
-        run_baseline(experiment)
+        _run_baseline(experiment)
         for aot in aots:
-            for prompt_name, prompt_path in prompt_approaches.items():
-                run_experiment(aot, prompt_name, prompt_path, experiment)
+            run_experiment(aot, parameters, experiment)
